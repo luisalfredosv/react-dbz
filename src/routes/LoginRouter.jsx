6a576ab46069f9844e6147d9a43260a9ad1a3a1b
luisalfredosv <luissarabia80@gmail.com>
2021-09-10 16:01:57 -0400

    Update LoginRouter.jsx

--- src/routes/LoginRouter.jsx
+++ src/routes/LoginRouter.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import LoginScreen from "../pages/LoginScreen";
 import AppRouter from "./AppRouter";
+import PrivateRouter from "./PrivateRouter";
+import PublicRouter from "./PublicRouter";
+
 const LoginRouter = () => {
+	const { log } = useContext(AuthContext);
+
 	return (
 		<Router>
-			<AppRouter />
+			{/* <AppRouter /> */}
+			{/* <Route exact path='/login' component={LoginScreen} /> */}
 			<Switch>
-				<Route exact path='/login' component={LoginScreen} />
+				<PublicRouter
+					path='/login'
+					auth={log}
+					component={LoginScreen}
+				/>
+
+				<PrivateRouter path='/' auth={log} component={AppRouter} />
 			</Switch>
 		</Router>
 	);
