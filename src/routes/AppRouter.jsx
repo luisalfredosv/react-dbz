83f161aa0e6e1132be5808d01fc826aef59ea9c1
luisalfredosv <luissarabia80@gmail.com>
2021-09-10 13:54:57 -0400

    Update AppRouter.jsx, LoginRouter.jsx, and PrivateRouter.jsx

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import ManScreen from "../pages/ManScreen";
+import WomanScreen from "../pages/WomanScreen";
+import SearchScreen from "../pages/SearchScreen";
+import CharacterScreen from "../pages/CharacterScreen";
+
+import NavBar from "../components/NavBar";
+
+const AppRouter = () => {
+	return (
+		<>
+			<NavBar />
+			<Switch>
+				<Route exact path='/mans' component={ManScreen} />
+				<Route exact path='/womans' component={WomanScreen} />
+				<Route exact path='/search' component={SearchScreen} />
+
+				<Route
+					exact
+					path='/character/:id'
+					component={CharacterScreen}
+				/>
+
+				<Redirect to='/login' />
+			</Switch>
+		</>
+	);
+};
+
+export default AppRouter;
