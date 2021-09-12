10066876e5a73aef12825253fe5d7bab2907d3c2
luisalfredosv <luissarabia80@gmail.com>
2021-09-12 17:50:49 -0400

    Update LoginScreen.jsx

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext"
+import { authTypes } from "../types/authtypes"
+import { useHistory } from "react-router"
 
-const LoginScreen = ({ history }) => {
+const LoginScreen = () => {
+
+	const { dispatch } = useContext(AuthContext)
+	const history = useHistory();
 	const handleLogin = () => {
-		history.push("/mans");
+		dispatch({type: authTypes.login})
+		history.push("/");
 	};
+	
 	return (
 		<div className='container mt-5 text-center'>
 			<img src='/assets/animate.gif' alt='animation' />
