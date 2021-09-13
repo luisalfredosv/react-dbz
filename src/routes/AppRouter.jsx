384b6b6aebf52092526659b8d7fef53c0cd46bf0
luisalfredosv <luissarabia80@gmail.com>
2021-09-13 09:48:13 -0400

    [Implement] lazy loading components

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-
-import ManScreen from "../pages/ManScreen";
-import WomanScreen from "../pages/WomanScreen";
-import SearchScreen from "../pages/SearchScreen";
-import CharacterScreen from "../pages/CharacterScreen";
-
+import { Container } from "react-bootstrap";
 import NavBar from "../components/NavBar";
+const ManScreen = lazy(() => import("../pages/ManScreen"));
+const WomanScreen = lazy(() => import("../pages/WomanScreen"));
+const SearchScreen = lazy(() => import("../pages/SearchScreen"));
+const CharacterScreen = lazy(() => import("../pages/CharacterScreen"));
 
 const AppRouter = () => {
 	return (
 		<>
 			<NavBar />
-			<Switch>
-				<Route exact path='/mans' component={ManScreen} />
-				<Route exact path='/womans' component={WomanScreen} />
-				<Route exact path='/search' component={SearchScreen} />
+			<Suspense
+				fallback={
+					<Container>
+						<h1>Loading...</h1>
+					</Container>
+				}
+			>
+				<Switch>
+					<Route exact path='/mans' component={ManScreen} />
+					<Route exact path='/womans' component={WomanScreen} />
+					<Route exact path='/search' component={SearchScreen} />
 
-				<Route
-					exact
-					path='/character/:id'
-					component={CharacterScreen}
-				/>
+					<Route
+						exact
+						path='/character/:id'
+						component={CharacterScreen}
+					/>
 
-				<Redirect to='/mans' />
-			</Switch>
+					<Redirect to='/mans' />
+				</Switch>
+			</Suspense>
 		</>
 	);
 };
